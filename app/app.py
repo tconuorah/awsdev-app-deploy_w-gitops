8286cc4edbe327b4dd42570b3bdad55388e79e71
tconuorah <Terrenceonuorah@gmail.com>
2026-01-22 23:58:54 -0600

    updated python script

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html", title="AWS DevOps")
+    return render_template("index.html", title="Welcome 1%'ers to the final lab!")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
